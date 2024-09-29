07fa01e12a609cb8cce0da52deee2641a6ac452c
david mbita <davidmbita001@gmail.com>
2024-09-29 13:05:37 -0700

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    HousingLocationComponent, // Non-standalone component
+    HousingLocationComponent,
+    DetailsComponent, // Non-standalone component
   ],
   imports: [
     BrowserModule,
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';  // Import RouterLink
 import { HousingLocation } from '../housinglocation';
 
 @Component({
@@ -7,13 +8,14 @@ import { HousingLocation } from '../housinglocation';
   <section class="listing">
     <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
     <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+    <p class="listing-location">{{ housingLocation.city}}, {{ housingLocation.state }}</p>
+    <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
   </section>
-  `,
+`,
   styleUrls: ['./housing-location.component.css'],
-  standalone:true
+  standalone: true,
+  imports: [RouterLink]  // Add RouterLink to the imports array
 })
 export class HousingLocationComponent {
   @Input() housingLocation!: HousingLocation;
-
 }
